6f64f6d083cda9b892c92a4b03f4db5032d35122
chandan1602 <cbansal82@gmail.com>
2019-08-24 12:19:45 +0530

    Manage-key-form-updated

--- src/views/Admin/Form.js
+++ src/views/Admin/Form.js
@@ -3,98 +3,95 @@ import { Button, Card, CardBody, CardFooter, Col, Container, Form, Input, InputG
 import { createKeyApi, authToken, userInfo } from '../../api';
 
 export default class Example extends React.Component {
-  constructor(props) {
-      super(props);
-      this.state = {
-        username: null,
-        email:null,
-        password: null,
-        accessid: null,
-        accesskey: null,
-        isLoading: false
-      }
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: null,
+            accessid: null,
+            accesskey: null,
+            isLoading: false
+        }
     }
 
     register = async () => {
-      console.log("register")
-      try {
-        let {
-          username,
-          password,
-          accesskey,
-          accessid,
-          email
-        } = this.state;
-        this.setState({
-          isLoading: true
-        })
-        console.log("state", this.state)
-        let token = await localStorage.getItem(authToken)
-        let createdby = await localStorage.getItem(userInfo)
-        let {user} = await createKeyApi.auth(`Bearer ${ token }`).post({accessid, accesskey, password, username, createdby, email})
-        console.log("usr", user)
-        this.props.history.goBack();
-        alert("user created")
-      } catch (error) {
-        console.log("err", error)
-        this.setState({
-          isLoading: false
-        })
-      }
+        console.log("register")
+        try {
+            let {
+                username,
+                password,
+                accesskey,
+                accessid,
+            } = this.state;
+            this.setState({
+                isLoading: true
+            })
+            console.log("state", this.state)
+            let token = await localStorage.getItem(authToken)
+            let createdby = await localStorage.getItem(userInfo)
+            let { user } = await createKeyApi.auth(`Bearer ${token}`).post({ accessid, accesskey, username, createdby })
+            console.log("usr", user)
+            this.props.history.goBack();
+            alert("user created")
+        } catch (error) {
+            console.log("err", error)
+            this.setState({
+                isLoading: false
+            })
+        }
     }
-  render() {
-    let { isLoading } = this.state
-    if(isLoading){
-      return (
-        <div className = "app flex-row align-items-center justify-content-center" >
-          <Spinner style={{width:"5rem", height:"5rem"}} color="primary"/>
-        </div>
-      )
-    }
-    return (
-      <Container>
-          <Row className="justify-content-center">
-            <Col md="10" lg="10" xl="10">
-              <Card className="mx-4">
-                <CardBody className="p-4">
-                  <Form>
-                    <h1 className="text-center" >Add Credentials </h1>
-                    <p className="text-muted text-center">Create Aws Setup</p>
-                    <InputGroup className="mb-3">
-                      <InputGroupAddon addonType="prepend">
-                        <InputGroupText>
-                          <i className="icon-user"></i>
-                        </InputGroupText>
-                      </InputGroupAddon>
-                      <Input type="text" placeholder="email" required onChange={e => this.setState({email:e.target.value})}/>
-                    </InputGroup>
-                    <InputGroup className="mb-3">
+    render() {
+        let { isLoading } = this.state
+        if (isLoading) {
+            return (
+                <div className="app flex-row align-items-center justify-content-center" >
+                    <Spinner style={{ width: "5rem", height: "5rem" }} color="primary" />
+                </div>
+            )
+        }
+        return (
+            <Container>
+                <Row className="justify-content-center">
+                    <Col md="10" lg="10" xl="10">
+                        <Card className="mx-4">
+                            <CardBody className="p-4">
+                                <Form>
+                                    <h1 className="text-center" >Add Credentials </h1>
+                                    <p className="text-muted text-center">Create Aws Setup</p>
+                                    <InputGroup className="mb-3">
+                                        <InputGroupAddon addonType="prepend">
+                                            <InputGroupText>
+                                                <i className="icon-user"></i>
+                                            </InputGroupText>
+                                        </InputGroupAddon>
+                                        <Input type="text" placeholder="Username" required onChange={e => this.setState({ username: e.target.value })} />
+                                    </InputGroup>
+                                    {/* <InputGroup className="mb-3">
                       <InputGroupAddon addonType="prepend">
                         <InputGroupText>
                           <i className="icon-lock"></i>
                         </InputGroupText>
                       </InputGroupAddon>
                       <Input type="password" placeholder="Password" required onChange={e => this.setState({password:e.target.value})}/>
-                    </InputGroup>
-                    <InputGroup className="mb-3">
-                      <InputGroupAddon addonType="prepend">
-                        <InputGroupText>@</InputGroupText>
-                      </InputGroupAddon>
-                      <Input type="text" placeholder="Access Id"required onChange={e => this.setState({accessid:e.target.value})}/>
-                    </InputGroup>
-                    <InputGroup className="mb-4">
-                      <InputGroupAddon addonType="prepend">
-                          <InputGroupText>#</InputGroupText>
-                      </InputGroupAddon>
-                      <Input type="text" placeholder="Access key" required onChange={e => this.setState({accesskey:e.target.value})}/>
-                    </InputGroup>
-                    <Button color="success" block onClick={this.register}>Create Credential</Button>
-                  </Form>
-                </CardBody>
-              </Card>
-            </Col>
-          </Row>
-        </Container>
-    );
-  }
+                    </InputGroup> */}
+                                    <InputGroup className="mb-3">
+                                        <InputGroupAddon addonType="prepend">
+                                            <InputGroupText>@</InputGroupText>
+                                        </InputGroupAddon>
+                                        <Input type="text" placeholder="Access Id" required onChange={e => this.setState({ accessid: e.target.value })} />
+                                    </InputGroup>
+                                    <InputGroup className="mb-4">
+                                        <InputGroupAddon addonType="prepend">
+                                            <InputGroupText>#</InputGroupText>
+                                        </InputGroupAddon>
+                                        <Input type="text" placeholder="Access key" required onChange={e => this.setState({ accesskey: e.target.value })} />
+                                    </InputGroup>
+                                    <Button color="success" block onClick={this.register}>Create Credential</Button>
+                                </Form>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+        );
+    }
 }
